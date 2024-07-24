375d62a1ec0ccd9f3a4d94d76eec974bb2e656b6
SyedDawood25 <dawoodsyedsie001@gmail.com>
2024-07-24 16:11:53 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -20,27 +20,25 @@ const Home = async () => {
           <SearchBar />
 
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter />
+            <CustomFilter />
           </div>
         </div>
 
-        {
-          !isDataEmpty ? (
-            <section>
-              <div className="home__cars-wrapper">
-                {allCars?.map((car) => (
-                  <CarCard car={car} />
-                ))}
-              </div>
-            </section>
-          ) : (
-            <div className="home__error-container">
-              <h2 className="text-black text-xl font-bold">Oops, no results</h2>
-              <p>{allCars?.message}</p>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
             </div>
-          )
-        }
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
